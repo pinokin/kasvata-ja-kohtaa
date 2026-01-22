35973f85abe2dd608235417a4b0980b42f8f49fa
Tuija <til.utilities@proton.me>
2026-01-22 13:25:39 +0200

    Navigaatio kuntoon ja tekstejä paikoilleen

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Kasvata ja kohtaa';
+export const SITE_DESCRIPTION = 'Jokainen lapsi ja nuori ansaitsee tulla nähdyksi ja ymmärretyksi. Myös silloin, kun arki tuo mukanaan haasteita.';
